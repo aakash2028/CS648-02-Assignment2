fcd0382fbe478eb534f11044d2ab23224287d1b2
aakash2028 <ajoshi3054@sdsu.edu>
2022-02-13 12:28:24 -0800

    Inital Commit

---
+++ public/app.js
@@ -0,0 +1,133 @@
+const Products = [];
+
+class ProductTable extends React.Component {
+  render() {
+    const productRows = this.props.products.map(product => /*#__PURE__*/React.createElement(ProductRow, {
+      key: product.id,
+      product: product
+    }));
+    return /*#__PURE__*/React.createElement("table", {
+      className: "table table-striped"
+    }, /*#__PURE__*/React.createElement("tr", {
+      className: "table"
+    }, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Price"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Image")), /*#__PURE__*/React.createElement("tbody", null, productRows));
+  }
+
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    return /*#__PURE__*/React.createElement("tr", {
+      className: "table"
+    }, /*#__PURE__*/React.createElement("td", null, product.productName), /*#__PURE__*/React.createElement("td", null, "$", product.productPrice), /*#__PURE__*/React.createElement("td", null, product.productCategory), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+      href: product.productImage,
+      target: "_blank"
+    }, "View")));
+  }
+
+}
+
+class AddProduct extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.productAdd;
+    var price = form.prdPrice.value;
+    price = price.slice(1);
+    const product = {
+      productName: form.prdName.value,
+      productPrice: price,
+      productCategory: form.prdCat.value,
+      productImage: form.prdImg.value
+    };
+    this.props.createProduct(product);
+    form.prdName.value = "";
+    form.prdPrice.value = "$";
+    form.prdImg.value = "";
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("form", {
+      name: "productAdd",
+      className: "formAdd",
+      onSubmit: this.handleSubmit
+    }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("label", null, "Category", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("select", {
+      id: "prdCat",
+      name: "category"
+    }, /*#__PURE__*/React.createElement("option", {
+      value: "shirts"
+    }, "Shirts"), /*#__PURE__*/React.createElement("option", {
+      value: "jeans"
+    }, "Jeans"), /*#__PURE__*/React.createElement("option", {
+      value: "jackets"
+    }, "Jackets"), /*#__PURE__*/React.createElement("option", {
+      value: "sweaters"
+    }, "Sweaters"), /*#__PURE__*/React.createElement("option", {
+      value: "accessories"
+    }, "Accessories")))), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("label", null, "Price Per Unit", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      type: "text",
+      name: "prdPrice",
+      defaultValue: "$"
+    }))), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("input", {
+      type: "submit",
+      id: "btnAdd",
+      value: "Add Product"
+    }))), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("label", null, "Product Name", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      type: "text",
+      name: "prdName"
+    }))), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("label", null, "Image URL", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+      type: "text",
+      name: "prdImg"
+    }))))));
+  }
+
+}
+
+class Product extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: []
+    };
+    this.createProduct = this.createProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({
+        products: Products
+      });
+    }, 500);
+  }
+
+  createProduct(product) {
+    product.id = this.state.products.length + 1;
+    const newProductList = this.state.products.slice();
+    newProductList.push(product);
+    this.setState({
+      products: newProductList
+    });
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "container jumbotron"
+    }, /*#__PURE__*/React.createElement("h1", null, "Inventory"), /*#__PURE__*/React.createElement("h3", null, "Showing all availble products"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement(ProductTable, {
+      products: this.state.products
+    }), " ", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h3", null, "Add a new product to inventory"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(AddProduct, {
+      createProduct: this.createProduct
+    }));
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Product, null), document.getElementById('section1'));
